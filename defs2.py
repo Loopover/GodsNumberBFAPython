0ebf46ca62c36ec7b82e5206910bbfd05cafd230
Bryce Wilson <bryce@thenetworknerds.ca>
2019-01-16 17:30:11 -0800

    Shortned some stuff by making everything harder to read

--- defs2.py
+++ defs2.py
@@ -4,44 +4,8 @@ import tracemalloc
 import os
 import linecache
 def switchTheStuff(board,currentNum,start,j,side):
-	if side == 1:
-		if currentNum == len(board)-1:
-			return (start[:j]+(board[0][j],)+start[j+1:])
-		else:
-			return (start[:j]+(board[currentNum+1][j],)+start[j+1:])
-	else:
-		if currentNum == 0:
-			return start[:j]+(board[-1][j],)+start[j+1:]
-		else:
-			return start[:j]+(board[currentNum-1][j],)+start[j+1:]
-def getFound(board, boards):
-	#print(board)
-	locnumber = 0
-	foundA = []
-	foundL = False
-	for j, line in enumerate(board):
-		for k in [1,-1]:
-			locnumber += 1
-			c = board[:j] + (line[k:]+line[:k],) + board[j+1:]
-			if c not in boards:
-				foundA.append(tuple(c))
-				#print(c)
-				foundL = True
-	for j in range(len(board)):
-		locnumber += 1
-		c = tuple(switchTheStuff(board,currentNum,start,j,1) for currentNum,start in enumerate(board))
-		if c not in boards:
-			foundA.append(tuple(c))
-			#print(c)
-			foundL = True
-	for j in range(len(board)):
-		locnumber += 1
-		c = tuple(switchTheStuff(board,currentNum,start,j,-1) for currentNum,start in enumerate(board))
-		if c not in boards:
-			foundA.append(tuple(c))
-			#print(c)
-			foundL = True
-	return (locnumber,foundA,foundL)
+	isSide = currentNum == ((-(side)^1)%3)*(len(board)-1)
+	return start[:j]+(board[int(isSide)*(-(((side)^1)%3))+int(not isSide)*(currentNum+side)][j],)+start[j+1:]
 def getNewPositions(board):
 	locnumber = 0
 	found = set()
